56ae1e7497f87ca6d22a754aea03f55e624ba635
SalnikovVladislav <70577567+SalnikovVladislav@users.noreply.github.com>
2020-09-01 13:54:12 +0300

    Create aga.cs

---
+++ aga.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Hi
+{
+    class programm{
+        static void Main(string[] args)
+        {
+            string hello = "Hello world";
+            Console.WriteLine(hello);
+        }
+    }
+    
+}
+
